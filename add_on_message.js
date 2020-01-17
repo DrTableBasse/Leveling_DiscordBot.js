83094b61c04f853be2a2cb0587ad760b71f6de6a
DrTableBasse <34471493+DrTableBasse@users.noreply.github.com>
2020-01-17 10:33:52 +0100

    Update add_on_message.js

--- add_on_message.js
+++ add_on_message.js
@@ -5,16 +5,20 @@ var profile = await dl.Fetch(message.author.id)
     await dl.SetXp(message.author.id, 0)
     message.reply(`Vous venez de level up!! Vous êtes maintenant level: ${profile.level + 1}`);
   }
+//repeat these lines for any role. 
+// You can change the number of what you want, level 1 -> role1 eg....
   if (profile.level +1 === 1) {
-      let petitdejeune = message.guild.roles.find(r => r.name === "Petit Déjeuner");
+      let petitdejeune = message.guild.roles.find(r => r.name === "role1");
     message.member.addRole(petitdejeune.id)
   } else if (profile.level + 1 === 10) {
-    let dejeune = message.guild.roles.find(r => r.name === "Déjeuner");
+    let dejeune = message.guild.roles.find(r => r.name === "role2");
     message.member.addRole(dejeune.id)
   } else if (profile.level + 1 === 20) {
-    let gouter = message.guild.roles.find(r => r.name === "Goûter");
+    let gouter = message.guild.roles.find(r => r.name === "role3");
     message.member.addRole(gouter.id)
   } else if (profile.level + 1 === 50) {
-    let Dîner = message.guild.roles.find(r => r.name === "Dîner");
+    let Dîner = message.guild.roles.find(r => r.name === "role4");
     message.member.addRole(gouter.id)
   }
+
+
